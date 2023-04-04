d42f3fad0ac5100cc6ae33d6417f9c095939c69e
AlanaSann <109287005+AlanaSann@users.noreply.github.com>
2023-04-03 23:20:35 -0300

    Add files via upload

---
+++ hereditario/Natimorto.java
@@ -0,0 +1,24 @@
+package hereditario;
+
+public class Natimorto extends Pessoa {
+
+	public Natimorto(Pessoa antecessor, String nome) {
+		super(antecessor, nome);
+		this.setNome("Natimorto" + nome);
+		if(antecessor != null) {
+			antecessor.getDescendentes().add(this);
+		}
+	}
+
+	public static Pessoa setNatimorto(Pessoa antecessor,String nome) { // metodo de acesso para o construtor filha.
+		return new Natimorto(antecessor,nome);
+		
+	}
+	
+	@Override
+	public Pessoa cadastrarFilhos(String nome, char sexo) {
+		Pessoa morto = setNatimorto(this,nome);
+		return morto;
+	}
+
+}
---
+++ hereditario/Pessoa.java
@@ -0,0 +1,47 @@
+package hereditario;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Pessoa {
+	private String nome;
+	private Pessoa antecessor;
+	private List<Pessoa> descendentes;
+	
+	public Pessoa(Pessoa antecessor,String nome) {
+		this.nome = nome;
+		this.antecessor = antecessor;
+		this.descendentes = new ArrayList<Pessoa>();
+	}
+
+	public List<Pessoa> getDescendentes() {
+		return descendentes;
+	}
+
+	public Pessoa getAntecessor() {
+		return antecessor;
+	}
+
+	public void setAntecessor(Pessoa antecessor) {
+		this.antecessor = antecessor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public abstract Pessoa cadastrarFilhos(String nome,char sexo);
+	
+	public void listarAntecessores(){
+		if(this.getAntecessor() != null) {
+			System.out.println(this.getAntecessor().getNome());
+			this.getAntecessor().listarAntecessores();
+		}
+	}
+
+
+}
